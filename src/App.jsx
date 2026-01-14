8cc1d955609d77b209ec93634d6f2fc603a1e3f1
stefanversterker <stefanversterker@gmail.com>
2026-01-14 19:33:44 +0100

    next page poging...alles kapot

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import Card from './Components/Card/Card.jsx';
 import AbilityContainer from './Components/AbilityContainer/AbilityContainer.jsx';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 
 function App() {
 
   const [data, setData] = useState([]);
-  const [error, toggleError] = useState(false);
+  /*const [error, toggleError] = useState(false);
   const [loading, toggleLoading] = useState(true);
   const [name, setName] = useState('');
   const [moves, setMoves] = useState(0);
   const [weight, setWeight] = useState(0);
   const [image, setImage] = useState('');
-  const [abilities, setAbilities] = useState('');
+  const [abilities, setAbilities] = useState('');*/
+  const [next, setNext] = useState(null);
+  const navigate = useNavigate();
+
 
 
   async function catchPokemon() {
 
     try {
-      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff');
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
 
       setData(response.data);
-      setName(response.data.name);
+      /*setName(response.data.name);
       setMoves(response.data.moves.length);
       setWeight(response.data.weight);
       setImage(response.data.sprites.front_default);
-      setAbilities(response.data.abilities);
-      console.log(response.data);
+      setAbilities(response.data.abilities);*/
+      setNext(response.data.next)
+      /*console.log(response.data);*/
 
     } catch (error) {
       console.error(error);
@@ -40,7 +45,13 @@ function App() {
 
   useEffect(() => {
     void catchPokemon();
-  },[]);
+  }, []);
+
+  useEffect(() => {
+    void catchPokemon();
+  }, [next]);
+
+
 
   return (
     <div className="everything-container blue-border">
@@ -48,40 +59,16 @@ function App() {
         <h1>Pokemon</h1>
       </header>
       <nav className="nav">
-        <button>vorige</button>
-        <button>volgende</button>
+        <button type="button" onClick={navigate(`${next}`)}>volgende</button>
       </nav>
       <main className="main-container green-border">
-        <Card pokeName={name}
-              pokeMovesAmount={moves}
-              pokeWeightAmount={weight}
-              pokeImage={image}
-        >
-          {data?.abilities?.map((a) => (
-            <li key={a.ability.name}>
-              <AbilityContainer pokeAbility={a.ability.name}/>
+        <ul>
+          {data?.results?.map((p) => (
+            <li key={p.name}>
+              <Card pokeName={p.name}></Card>
             </li>
-            ))}
-        </Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+          ))}
+        </ul>
       </main>
     </div>
   );
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,16 +1,59 @@
 import './Card.css';
+import AbilityContainer from '../AbilityContainer/AbilityContainer.jsx';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
+
 
 function Card({pokeName, pokeMovesAmount, pokeWeightAmount, pokeImage, children}) {
 
+  const [data, setData] = useState([]);
+  const [error, toggleError] = useState(false);
+  const [loading, toggleLoading] = useState(true);
+  const [name, setName] = useState('');
+  const [moves, setMoves] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [image, setImage] = useState('');
+  const [abilities, setAbilities] = useState('');
+
+  async function catchPokemon() {
+
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+
+      setData(response.data);
+      setName(response.data.name);
+      setMoves(response.data.moves.length);
+      setWeight(response.data.weight);
+      setImage(response.data.sprites.front_default);
+      setAbilities(response.data.abilities);
+      /*console.log(response.data);*/
+
+    } catch (error) {
+      console.error(error);
+      toggleError(true);
+    } finally {
+      toggleLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    void catchPokemon();
+  },[]);
+
+
   return (
     <article className="card">
-      <h3>{pokeName}</h3>
-      <div className="poke-image pink-border"><img src={pokeImage} alt=""/></div>
-      <h4>Moves: {pokeMovesAmount}</h4>
-      <h4>Weight: {pokeWeightAmount}</h4>
+      <h3>{name}</h3>
+      <div className="poke-image pink-border"><img src={image} alt=""/></div>
+      <h4>Moves: {moves}</h4>
+      <h4>Weight: {weight}</h4>
       <h4>Abilities:</h4>
       <ul className="ability-list">
-        {children}
+        {data?.abilities?.map((a) => (
+          <li key={a.ability.name}>
+            <AbilityContainer pokeAbility={a.ability.name}/>
+          </li>
+        ))}
       </ul>
     </article>
   );
